2475bd115acb2b11ab6ad9b1b4f85a4e5665cbbc
tauraamui <adamplewis@pm.me>
2021-01-12 09:09:01 +0000

    Use std template parser to load login page content into template html

--- web/login_handler.go
+++ web/login_handler.go
@@ -1,23 +1,47 @@
 package web
 
 import (
+	"html/template"
 	"net/http"
 	"sync"
 )
 
+type TemplateData struct {
+	Title   string
+	Content template.HTML
+}
+
 func (s *Server) handleLoginGet() http.HandlerFunc {
 
 	var (
-		init sync.Once
-		p    string
-		err  error
+		init         sync.Once
+		templateHTML string
+		pageHTML     string
+		data         TemplateData
+		tmpl         *template.Template
+		err          error
 	)
 
 	return func(w http.ResponseWriter, r *http.Request) {
 		init.Do(func() {
-			p, err = s.htmlRiceBox.String("login/index.html")
+			templateHTML, err = s.htmlRiceBox.String("template.html")
 			if err != nil {
-				s.errlog.Printf("Unable to load login page HTML")
+				s.errlog.Printf("Unable to load template HTML: %v\n", err)
+			}
+
+			pageHTML, err = s.htmlRiceBox.String("login/index.html")
+			if err != nil {
+				s.errlog.Printf("Unable to load login page HTML: %v\n", err)
+			}
+
+			data = TemplateData{
+				Title:   "Login",
+				Content: template.HTML(pageHTML),
+			}
+
+			tmpl, err = template.New("template").Parse(templateHTML)
+			if err != nil {
+				s.errlog.Printf("Unable to load template with login page content: %v\n", err)
 			}
 		})
 
@@ -27,6 +51,8 @@ func (s *Server) handleLoginGet() http.HandlerFunc {
 			return
 		}
 
-		w.Write([]byte(p))
+		if tmpl != nil {
+			tmpl.Execute(w, data)
+		}
 	}
 }
